eda4faf97a8dad5ca58491df1d8c77e71188b4b3
ckmihara <ckmihara@gmail.com>
2022-11-22 22:20:41 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/ckmihara/workshopmongo/config/instatiation.java
@@ -0,0 +1,31 @@
+package com.ckmihara.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ckmihara.workshopmongo.domain.User;
+import com.ckmihara.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class instatiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}

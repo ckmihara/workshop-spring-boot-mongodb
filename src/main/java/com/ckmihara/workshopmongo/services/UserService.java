d1731114f60a55769bd9f71c6040853e58538a5c
ckmihara <ckmihara@gmail.com>
2022-11-22 21:47:39 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ckmihara/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.ckmihara.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ckmihara.workshopmongo.domain.User;
+import com.ckmihara.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}

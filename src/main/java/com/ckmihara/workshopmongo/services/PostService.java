f92792e6a21ee10135581ba9d16fbb10563def7f
ckmihara <ckmihara@gmail.com>
2022-11-25 22:16:46 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ckmihara/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.ckmihara.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ckmihara.workshopmongo.domain.Post;
+import com.ckmihara.workshopmongo.repository.PostRepository;
+import com.ckmihara.workshopmongo.servicesexception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
